495c026173d3347e95829fe575463ecdf7703c47
Cyberkrane <cyberkrane@gmail.com>
2023-05-05 10:49:30 -0300

    agregue angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { SharedModule } from './shared/shared.module';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsPageComponent,
+    NavBarComponent,
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
